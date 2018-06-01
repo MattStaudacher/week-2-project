d1c6016c916d033aab65fb514a8059ae63aabedc
Matt Staudacher <mattstaudacher3@gmail.com>
2018-06-01 10:43:31 -0700

    it will add javascript variables.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
-$
+$(document).ready(function() {
+  $("form#track").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+    var industry = $("select#industry").val();
+    var thrones = $("select#thrones").val();
+    var drink = $("input:radio[name=drink]:checked").val();
+
+
+
+  if (age < 25) {
+    $('#csharp').show();
+  } else if (gender === "male" && age < 25) {
+    $('#cssreact').show();
+  }
+
+
+
+
+
+
+
+    event.preventDefault();
+  });
+});
